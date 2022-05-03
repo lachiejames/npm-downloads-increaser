49c78cf91308054c09fa845247b19fba5ed31e36
Lachie James <103395442+lachiejamesCTC@users.noreply.github.com>
2022-05-04 03:25:06 +1000

    fix: update configs and dependencies (#92)

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,4 @@
 module.exports = {
-    collectCoverageFrom: [
-        "src/**/*.ts",
-        "!**/node_modules*/**",
-        "!<rootDir>/coverage*/**",
-        "!<rootDir>/dist*/**",
-        "!<rootDir>/lib*/**",
-        "!<rootDir>/tmp*/**",
-        "!**/index.ts",
-        "!**/cli.ts",
-    ],
-    coverageDirectory: "coverage",
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: -10,
-        },
-    },
-    globals: {
-        window: {},
-    },
-    moduleNameMapper: {
-        "^src$": "<rootDir>/src",
-        "^src/(.+)$": "<rootDir>/src/$1",
-    },
     preset: "ts-jest",
-    testPathIgnorePatterns: ["/node_modules.*/", "<rootDir>/(coverage|dist|lib|tmp).*/"],
-    reporters: ["default"],
+    testPathIgnorePatterns: ["<rootDir>/(coverage|lib).*/"],
 };
